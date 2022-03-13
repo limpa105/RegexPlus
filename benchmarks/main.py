9be46d1296d347b08fa9cf9164889406199427f5
lhimpa105 <liza@pertseva.com>
2022-03-12 17:11:13 -0800

    added a timeout function

--- benchmarks/main.py
+++ benchmarks/main.py
@@ -1,11 +1,40 @@
 import synthesis
 import examples
 import random
+import os
+import signal
+import time
+import gc
 
 global USE_OPTIONALS
 USE_OPTIONALS = True
 
 NUM_EXAMPLES = 4
+TIMEOUT_SEC = 40 
+
+
+
+
+# Using code from https://erogol.com/timeout-function-takes-long-finish-python/
+class TimeoutError(Exception):
+    pass
+
+class timeout:
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError()
+        gc.collect()
+        print("Hello?")
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
+
+
+
 
 print("doin' VSA stuff")
 for i, ex in enumerate(examples.all_benchmarks):
@@ -13,6 +42,11 @@ for i, ex in enumerate(examples.all_benchmarks):
     print(ex.inputs[:NUM_EXAMPLES])
     print(f'  correct: {ex.regex}')
     print('  synthesized:')
-    for wt, regex in synthesis.synthesize(ex.inputs[:NUM_EXAMPLES]):
-        print('   - [%.3f] %s' % (wt, regex))
+    with timeout(seconds=TIMEOUT_SEC):
+        try:
+    	    for wt, regex in synthesis.synthesize(ex.inputs[:NUM_EXAMPLES]):
+        	    print('   - [%.3f] %s' % (wt, regex))
+        except:
+            print("Timed out")
+            pass
 
